3784e339ccaf52c680ed67059700d7bd6423c1a6
WAR-MACHINE\krk86 <krk86.mca@gmail.com>
2023-02-05 13:04:28 +0530

    initial push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SiderbarComponent } from './common/components/siderbar/siderbar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiderbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
