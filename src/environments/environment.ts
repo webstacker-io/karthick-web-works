a6c8e5a4d96c28593b60509c3c63224c9bfaee07
Karthick <webstacker.io@gmail.com>
2023-12-13 09:53:44 +0530

    env vars updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  space: process.env["space"],
-  accessToken: process.env["accessToken"],
-  contentId: process.env["contentId"]
+  space: process.env["space"] || '',
+  accessToken: process.env["accessToken"] || '',
+  contentId: process.env["contentId"] || ''
 };
